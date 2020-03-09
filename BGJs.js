cf437cd6d3889abe0d3be94d0a235a590778eb1d
kevindev10 <kevindev1@outlook.com>
2020-03-09 12:41:25 +0300

    Addition of new files related to npm

--- BGJs.js
+++ BGJs.js
@@ -1,3 +1,10 @@
+
+var _ = require('lodash');
+
+var array = [1,2,3,4,5,6,7,8];
+console.log('answer', _.without(array,5) )
+
+
 var color1=document.getElementById("color1");
 var color2=document.getElementById("color2");
 var body =document.getElementById("body");
